3d79495090dac07573d91704a8e1f776f2677881
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-10-16 21:05:09 -0300

    Second Commit ( adding validation)

--- FavoriteNumber/FavoriteNumber/main.cpp
+++ FavoriteNumber/FavoriteNumber/main.cpp
@@ -1,6 +1,7 @@
 //A simple program to practice read and write operations
 
 #include<iostream>;
+#include "main.h"
 
 
 int main()
@@ -8,25 +9,27 @@ int main()
 	int favoriteNumber = 0;
 
 
+
+	FavoriteNumberInput(favoriteNumber);
+
+	while ((favoriteNumber >= 100 || favoriteNumber <= 0))
+	{
+		std::cout << "Incorrect entry, please write a number between 1 and 100" << std::endl;
+		std::cin >> favoriteNumber;
+	}
+
+
+	std::cout << "Awesome, that's my favorite number too !" << std::endl;
+	std::cout << "No really!!," << favoriteNumber << " is my favorite Number! " << std::endl;
+
+return 0;
+}
+
+void FavoriteNumberInput(int& favoriteNumber)
+{
 	std::cout << "What is your favorite number between 1 and 100 ?" << std::endl;
 
 	std::cin >> favoriteNumber;
-
-
-if (favoriteNumber >= 100 || favoriteNumber > 0)
-{
-	std::cout << "Incorrect entry, please write a number between 1 and 100" << std::endl;
-
-}
-
-else
-{
-	std::cout << "Awesome, that's my favorite number too !" << std::endl;
-	std::cout << "No really!!," + favoriteNumber; std::cout << "is my favorite Number! " << std::endl;
-
-}
-
-return 0;
 }
 
 
